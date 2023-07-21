55b71ee0e8decce7266af97cb9e045bc5e047075
Yahya KARAYEL <karayelyahya@gmail.com>
2023-07-21 14:19:24 +0300

    AspNetAPI work

---
+++ Web/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+ 
