deb7e58306a10e474235de5e23a34bf3aad805d6
Yahya KARAYEL <karayelyahya@gmail.com>
2023-07-21 17:20:00 +0300

    Fix API to new MVC

--- NLayer.Core/Services/IProductService.cs
+++ NLayer.Core/Services/IProductService.cs
@@ -10,6 +10,6 @@ namespace NLayer.Core.Services
 {
     public interface IProductService : IService<Product>
     {
-        Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductWithCategory();
+        Task<List<ProductWithCategoryDto>> GetProductWithCategory();
     }
 }
--- NLayer.Service/ProductService.cs
+++ NLayer.Service/ProductService.cs
@@ -13,22 +13,22 @@ using System.Threading.Tasks;
 
 namespace NLayer.Service
 {
-    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    public class ProductService : Service<Product>, IProductService
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
-        public ProductServiceWithNoCaching(IUnitOfWork unitOfWork, IGenericRepository<Product> repository, IProductRepository productRepository, IMapper mapper) : base(unitOfWork, repository)
+        public ProductService(IUnitOfWork unitOfWork, IGenericRepository<Product> repository, IProductRepository productRepository, IMapper mapper) : base(unitOfWork, repository)
         {
             _productRepository = productRepository;
             _mapper = mapper;
         }
 
-        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductWithCategory()
+        public async Task<List<ProductWithCategoryDto>> GetProductWithCategory()
         {
             var products = await _productRepository.GetProductWithCategory();
             
             var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
-            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+            return productsDto;
         }
     }
 }
