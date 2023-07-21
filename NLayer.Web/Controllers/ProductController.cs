a272bdc72be6511803ab2bf45cb5688cba3f5a9c
Yahya KARAYEL <karayelyahya@gmail.com>
2023-07-21 16:44:53 +0300

    AspNetMVC

---
+++ NLayer.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _services;
+
+        public ProductController(IProductService services)
+        {
+            _services = services;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _services.GetProductWithCategory()); 
+        }
+    }
+}
