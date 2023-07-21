55c53a80637bf0aade346690e2d473aa7009d232
Yahya KARAYEL <karayelyahya@gmail.com>
2023-07-21 14:25:23 +0300

    AspNetApi Final

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -41,6 +41,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 });
 
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
